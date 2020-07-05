82941e6da2caf75f7626e5b253f881ab29175971
adicomdotir <adicom.ir@gmail.com>
2020-07-05 23:46:03 +0430

    Add logic to color to hex project

--- app/src/main/java/ir/adicom/app/beginneridea/MainActivity.kt
+++ app/src/main/java/ir/adicom/app/beginneridea/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import ir.adicom.app.beginneridea.bin2dec.Bin2DecActivity
+import ir.adicom.app.beginneridea.rgb2hex.ColorToHexActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +13,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bin2dec = findViewById<Button>(R.id.btn_bin2dec)
-        bin2dec.setOnClickListener {
+        val bin2decBtn = findViewById<Button>(R.id.btn_bin2dec)
+        bin2decBtn.setOnClickListener {
             val intent = Intent(this, Bin2DecActivity::class.java)
+            startActivity(intent)
+        }
+
+        val rgb2hexBtn = findViewById<Button>(R.id.btn_rgb2hex)
+        rgb2hexBtn.setOnClickListener {
+            val intent = Intent(this, ColorToHexActivity::class.java)
             startActivity(intent)
         }
     }
---
+++ app/src/main/java/ir/adicom/app/beginneridea/rgb2hex/ColorToHexActivity.kt
@@ -0,0 +1,67 @@
+package ir.adicom.app.beginneridea.rgb2hex
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import ir.adicom.app.beginneridea.R
+
+class ColorToHexActivity : AppCompatActivity() {
+
+    lateinit var btnCalculate: Button
+    lateinit var edtRed: EditText
+    lateinit var edtGreen: EditText
+    lateinit var edtBlue: EditText
+    lateinit var tvResult: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_color_to_hex)
+
+        edtBlue = findViewById(R.id.edt_blue)
+        edtGreen = findViewById(R.id.edt_green)
+        edtRed = findViewById(R.id.edt_red)
+        tvResult = findViewById(R.id.tv_result)
+        btnCalculate = findViewById(R.id.btn_calculate)
+
+        btnCalculate.setOnClickListener {
+            val sb = StringBuilder()
+            val redValue = edtRed.text.toString().toInt()
+            val greenValue = edtGreen.text.toString().toInt()
+            val blueValue = edtBlue.text.toString().toInt()
+            if (redValue < 0 || redValue > 255 || greenValue < 0 || greenValue > 255 || blueValue < 0 || blueValue > 255) {
+                Toast.makeText(this, "Value must to be 0-255", Toast.LENGTH_SHORT).show()
+            } else {
+                sb.append(decimalToHex(redValue))
+                sb.append(decimalToHex(greenValue))
+                sb.append(decimalToHex(blueValue))
+                tvResult.text = sb.toString()
+            }
+        }
+    }
+
+    fun decimalToHex(value: Int): String {
+        val sb = StringBuilder()
+        var newValue = value
+        while (newValue > 0) {
+            val res = newValue % 16;
+            newValue /= 16
+            sb.append(numberToDigit(res))
+        }
+        return sb.reverse().toString()
+    }
+
+    private fun numberToDigit(value: Int): String {
+        when (value) {
+            10 -> return "A"
+            11 -> return "B"
+            12 -> return "C"
+            13 -> return "D"
+            14 -> return "E"
+            15 -> return "F"
+        }
+        return value.toString()
+    }
+}
