fc3dfd71fa8b57a68da35fccd0b773bfe83c2d9f
adicomdotir <adicom.ir@gmail.com>
2020-07-17 23:17:04 +0430

    Add score activity

--- app/src/main/java/ir/adicom/app/beginneridea/quiz/EndGameActivity.kt
+++ app/src/main/java/ir/adicom/app/beginneridea/quiz/EndGameActivity.kt
@@ -1,6 +1,7 @@
 package ir.adicom.app.beginneridea.quiz
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import ir.adicom.app.beginneridea.R
 
@@ -9,5 +10,10 @@ class EndGameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_end_game)
+
+        val tvResult = findViewById<TextView>(R.id.tv_result)
+        val score = intent.extras.getInt("score").toString()
+        tvResult.text = "Your Score is $score"
+
     }
 }
--- app/src/main/java/ir/adicom/app/beginneridea/quiz/SimpleQuizActivity.kt
+++ app/src/main/java/ir/adicom/app/beginneridea/quiz/SimpleQuizActivity.kt
@@ -17,44 +17,23 @@ class SimpleQuizActivity : AppCompatActivity() {
     lateinit var answerThree: Button
     lateinit var answerFour: Button
     lateinit var tvScore: TextView
+    lateinit var answers: MutableList<Int>
+    lateinit var tvQuestion: TextView
     var correctAnswer: Int = 0
     var score: Int = 0
+    var questionRandomIndex: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_simple_quiz)
 
-        var rand: Int = floor(Math.random() * countries.size).toInt()
-        while (rand % 2 == 1) {
-            rand = floor(Math.random() * countries.size).toInt()
-        }
-        val tvQuestion = findViewById<TextView>(R.id.tv_question)
-        tvQuestion.text = "What is capital of ${countries[rand]}?"
-
-        val answers = mutableListOf<Int>()
-        correctAnswer = rand + 1
-        answers.add(rand + 1)
-        for (i in 1..3) {
-            var answer = floor(Math.random() * countries.size).toInt()
-            while (answer % 2 == 0 || answers.contains(answer)) {
-                answer = floor(Math.random() * countries.size).toInt()
-            }
-            answers.add(answer)
-        }
-        answers.shuffle()
-
+        tvQuestion = findViewById(R.id.tv_question)
         answerOne = findViewById(R.id.btn_one)
-        answerOne.text = countries[answers[0]]
-        answerOne.tag = answers[0]
         answerTwo = findViewById(R.id.btn_two)
-        answerTwo.text = countries[answers[1]]
-        answerTwo.tag = answers[1]
         answerThree = findViewById(R.id.btn_three)
-        answerThree.text = countries[answers[2]]
-        answerThree.tag = answers[2]
         answerFour = findViewById(R.id.btn_four)
-        answerFour.text = countries[answers[3]]
-        answerFour.tag = answers[3]
+
+        generateQuestion()
 
         answerOne.setOnClickListener(::btnOnClick)
         answerTwo.setOnClickListener(::btnOnClick)
@@ -65,17 +44,51 @@ class SimpleQuizActivity : AppCompatActivity() {
         tvScore.text = "Score: $score"
     }
 
-    fun btnOnClick(l: View) {
+    private fun btnOnClick(l: View) {
         val btn = findViewById<Button>(l.id)
         if (l.getTag() == correctAnswer) {
             btn.setTextColor(Color.GREEN)
             score += 10
             tvScore.text = "Score: $score"
+            generateQuestion()
         } else {
             btn.setTextColor(Color.RED)
             val intent = Intent(this, EndGameActivity::class.java)
             intent.putExtra("score", score)
             startActivity(intent)
         }
+    }
+
+    private fun generateQuestion() {
+        questionRandomIndex = floor(Math.random() * countries.size).toInt()
+        while (questionRandomIndex % 2 == 1) {
+            questionRandomIndex = floor(Math.random() * countries.size).toInt()
+        }
+
+        answers = mutableListOf()
+        correctAnswer = questionRandomIndex + 1
+        answers.add(questionRandomIndex + 1)
+        for (i in 1..3) {
+            var answer = floor(Math.random() * countries.size).toInt()
+            while (answer % 2 == 0 || answers.contains(answer)) {
+                answer = floor(Math.random() * countries.size).toInt()
+            }
+            answers.add(answer)
+        }
+        answers.shuffle()
+
+        tvQuestion.text = "What is capital of ${countries[questionRandomIndex]}?"
+        answerOne.text = countries[answers[0]]
+        answerOne.tag = answers[0]
+        answerOne.setTextColor(Color.BLACK)
+        answerTwo.text = countries[answers[1]]
+        answerTwo.tag = answers[1]
+        answerTwo.setTextColor(Color.BLACK)
+        answerThree.text = countries[answers[2]]
+        answerThree.tag = answers[2]
+        answerThree.setTextColor(Color.BLACK)
+        answerFour.text = countries[answers[3]]
+        answerFour.tag = answers[3]
+        answerFour.setTextColor(Color.BLACK)
     }
 }
