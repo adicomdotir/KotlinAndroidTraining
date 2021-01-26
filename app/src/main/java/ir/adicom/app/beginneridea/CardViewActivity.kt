4de97b962bb2f5863fe9e41f42e82ea31c389b51
adicomdotir <adicom.ir@gmail.com>
2021-01-26 23:15:03 +0330

    Add card view example

---
+++ app/src/main/java/ir/adicom/app/beginneridea/CardViewActivity.kt
@@ -0,0 +1,11 @@
+package ir.adicom.app.beginneridea
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CardViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_view)
+    }
+}
