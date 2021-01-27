87914bb181d48f374d5423f1b02a30181f30f33a
adicomdotir <adicom.ir@gmail.com>
2021-01-28 00:19:21 +0330

    Add card view example 2

---
+++ app/src/main/java/ir/adicom/app/beginneridea/ModernActivity.kt
@@ -0,0 +1,11 @@
+package ir.adicom.app.beginneridea
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ModernActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modern)
+    }
+}
