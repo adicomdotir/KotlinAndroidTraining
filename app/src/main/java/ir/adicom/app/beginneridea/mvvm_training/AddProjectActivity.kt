db0ce8cbf4dfdf947c305f915ff08f9862210cd8
adicomdotir <adicom.ir@gmail.com>
2021-03-08 10:12:35 +0330

    Add xml files

---
+++ app/src/main/java/ir/adicom/app/beginneridea/mvvm_training/AddProjectActivity.kt
@@ -0,0 +1,11 @@
+package ir.adicom.app.beginneridea.mvvm_training
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddProjectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_project)
+    }
+}
---
+++ app/src/main/java/ir/adicom/app/beginneridea/mvvm_training/MainTrainingActivity.kt
@@ -0,0 +1,11 @@
+package ir.adicom.app.beginneridea.mvvm_training
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainTrainingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_training)
+    }
+}
