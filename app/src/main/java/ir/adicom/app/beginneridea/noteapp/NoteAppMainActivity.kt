007b82b88c8eafad70d5507fb029866567d09f7c
adicomdotir <adicom.ir@gmail.com>
2021-03-12 01:11:30 +0330

    Add note app ui

---
+++ app/src/main/java/ir/adicom/app/beginneridea/noteapp/NoteAppMainActivity.kt
@@ -0,0 +1,12 @@
+package ir.adicom.app.beginneridea.noteapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ir.adicom.app.beginneridea.R
+
+class NoteAppMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_app_main)
+    }
+}
