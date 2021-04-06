1cffeb59418ee7145d24a12828e658a66bd4aefc
adicomdotir <adicom.ir@gmail.com>
2021-04-06 23:41:50 +0430

    Add other ui for login

---
+++ app/src/main/java/ir/adicom/app/beginneridea/login/OtherLoginActivity.kt
@@ -0,0 +1,12 @@
+package ir.adicom.app.beginneridea.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.adicom.app.beginneridea.R
+
+class OtherLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other_login)
+    }
+}
